b6457ec366f3b89d15b53ad05ecc48bc734e881a
Bakhmatov_Serhii <bakhmatov1982@gmail.com>
2025-07-16 11:22:41 +0300

    add layout.tsx

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -1,8 +1,8 @@
 // app/(auth routes)/layout.tsx
 // або app/(private routes)/layout.tsx
 
-// import { ReactNode } from "react";
+import { ReactNode } from "react";
 
-// export default function Layout({ children }: { children: ReactNode }) {
-//   return <>{children}</>;
-// }
+export default function Layout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
